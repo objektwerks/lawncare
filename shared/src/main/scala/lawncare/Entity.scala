1b484fe904fb36bd6d2efb9a5f61630f2cc9130f
objektwerks <objektwerks@runbox.com>
2024-04-02 14:56:07 -0400

    edited entity

--- shared/src/main/scala/lawncare/Entity.scala
+++ shared/src/main/scala/lawncare/Entity.scala
@@ -6,6 +6,6 @@ edging ( driveway, curbing )
 trimming ( plant, tree )
 fertilizing ( nitrogen, phosphorus and potassium )
 pesticides ( insecticides, herbicides and fungicides )
-watering
+watering ( sprinkler system, pipes, heads, on/off | uncapped/capped )
 */
 sealed trait Entity
