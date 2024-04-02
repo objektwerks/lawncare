6fc50801a448e75dea38a961dad82e929270b880
objektwerks <objektwerks@runbox.com>
2024-04-02 12:38:13 -0400

    enhanced pin test

--- shared/src/test/scala/lawncare/PinTest.scala
+++ shared/src/test/scala/lawncare/PinTest.scala
@@ -3,7 +3,14 @@ package lawncare
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.mutable
+
 final class PinTest extends AnyFunSuite with Matchers:
   test("pin uuid"):
+    val pins = mutable.Set.empty[String]
     for i <- 1 to 1_000_000
-    do Pin.newPin.length shouldBe 7
+    do
+      val pin = Pin.newPin
+      pin.length shouldBe 7
+      pins += pin
+    pins.size shouldBe 1_000_000
