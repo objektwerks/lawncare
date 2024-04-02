07e24940e6761372c5af7f66039a56847300e6f4
objektwerks <objektwerks@runbox.com>
2024-04-02 12:23:40 -0400

    enhanced pin test

--- shared/src/test/scala/lawncare/PinTest.scala
+++ shared/src/test/scala/lawncare/PinTest.scala
@@ -4,6 +4,10 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 final class PinTest extends AnyFunSuite with Matchers:
-  test("pin"):
+  test("pin random"):
     for i <- 1 to 1_000_000
     do Pin.newInstance.length shouldBe 7
+
+  test("pin uuid")
+    for i <- 1 to 1_000_000
+    do Pin.newPin.length shouldBe 7
