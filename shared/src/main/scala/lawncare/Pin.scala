dbff3bef34f6bc5d749cd979f4cf8dadce015d76
objektwerks <objektwerks@runbox.com>
2024-04-02 12:32:52 -0400

    simplified pin and pin test

--- shared/src/main/scala/lawncare/Pin.scala
+++ shared/src/main/scala/lawncare/Pin.scala
@@ -2,22 +2,5 @@ package lawncare
 
 import java.util.UUID
 
-import scala.util.Random
-
 object Pin:
-  private val specialChars = "~!@#$%^&*-+=<>?/:;".toList
-  private val random = Random
-
-  private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
-
-  def newInstance: String =
-    Random.shuffle(
-      Random
-        .alphanumeric
-        .take(5)
-        .mkString
-        .prepended(newSpecialChar)
-        .appended(newSpecialChar)
-    ).mkString
-
   def newPin: String = UUID.randomUUID().toString().substring(0, 7).toLowerCase()
