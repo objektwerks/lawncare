1701f485636e37d0436a703f9154a61f86f98d20
objektwerks <objektwerks@runbox.com>
2024-04-02 15:11:20 -0400

    refactored entity

--- shared/src/main/scala/lawncare/Entity.scala
+++ shared/src/main/scala/lawncare/Entity.scala
@@ -11,12 +11,12 @@ water ( sprinkler system, pipes, heads, on/off | uncapped/capped )
 sealed trait Entity:
   val id: Long
 
-final case class Mowing(id: Long = 0) extends Entity
+final case class Mow(id: Long = 0) extends Entity
 
-final case class Trimming(id: Long = 0) extends Entity
+final case class Trim(id: Long = 0) extends Entity
 
-final case class Fertilizing(id: Long = 0) extends Entity
+final case class Fertilize(id: Long = 0) extends Entity
 
-final case class Pesticiding(id: Long = 0) extends Entity
+final case class Pesticide(id: Long = 0) extends Entity
 
-final case class Watering(id: Long = 0) extends Entity
+final case class Water(id: Long = 0) extends Entity
