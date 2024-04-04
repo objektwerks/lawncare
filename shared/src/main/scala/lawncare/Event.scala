da5c022bd8fd0797cec93d7a869dd7278eb6043d
objektwerks <objektwerks@runbox.com>
2024-04-04 11:39:37 -0400

    added fault event

--- shared/src/main/scala/lawncare/Event.scala
+++ shared/src/main/scala/lawncare/Event.scala
@@ -1,3 +1,30 @@
 package lawncare
 
+import java.time.{Instant, LocalDate}
+
+import scalafx.beans.property.ObjectProperty
+
 sealed trait Event
+
+object Fault:
+  def apply(throwable: Throwable, defaultMessage: String): Fault =
+    val message = throwable.getMessage
+    Fault(
+      if message == null then defaultMessage
+      else message
+    )
+
+  def apply(prefixMessage: String, throwable: Throwable): Fault =
+    val message = throwable.getMessage
+    Fault(
+      if message == null then prefixMessage
+      else s"$prefixMessage $message"
+    )
+
+  given faultOrdering: Ordering[Fault] = Ordering.by[Fault, Long](f => LocalDate.parse(f.occurred).toEpochDay()).reverse
+
+final case class Fault (cause: String, occurred: String = Instant.now.toString) extends Event:
+  val causeProperty = ObjectProperty[String](this, "cause", cause)
+  val occurredProperty = ObjectProperty[String](this, "occurred", occurred)
+
+final case class FaultAdded() extends Event
