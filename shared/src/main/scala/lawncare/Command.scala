497715589b4953851d40079651db4ead8a5a4deb
objektwerks <objektwerks@runbox.com>
2024-04-04 11:26:38 -0400

    enhanced command

--- shared/src/main/scala/lawncare/Command.scala
+++ shared/src/main/scala/lawncare/Command.scala
@@ -3,3 +3,4 @@ package lawncare
 sealed trait Command
 
 final case class Register(email: String, location: String) extends Command
+final case class Login(email: String, pin: String) extends Command
