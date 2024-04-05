c143cff432a44e75c8c6764259a91ca774ec3016
objektwerks <objektwerks@runbox.com>
2024-04-05 06:53:57 -0400

    edited account table

--- ddl.sql
+++ ddl.sql
@@ -5,7 +5,8 @@ CREATE TABLE account (
   id BIGSERIAL PRIMARY KEY,
   license CHAR(36) UNIQUE NOT NULL,
   email VARCHAR UNIQUE NOT NULL,
-  pin CHAR(7) NOT NULL
+  pin CHAR(7) NOT NULL,
+  activated VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE property (
