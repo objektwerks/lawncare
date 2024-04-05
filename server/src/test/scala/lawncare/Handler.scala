b874ec2c09db637b10773c06f1aef93a19d6f2d5
objektwerks <objektwerks@runbox.com>
2024-04-05 08:13:35 -0400

    added handler

---
+++ server/src/test/scala/lawncare/Handler.scala
@@ -0,0 +1,29 @@
+package lawncare
+
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.typesafe.scalalogging.Logger
+
+import io.helidon.webserver.http.{Handler => WebHandler, ServerRequest, ServerResponse}
+
+final class Handler(dispatcher: Dispatcher,
+                    store: Store,
+                    logger: Logger) extends WebHandler:
+  override def handle(request: ServerRequest,
+                      response: ServerResponse): Unit =
+    val commandJson = request.content.as(classOf[String])
+    val command = readFromString[Command](commandJson)
+    logger.debug(s"*** Handler command: $command")
+
+    val event = dispatcher.dispatch(command)
+    logger.debug(s"*** Handler event: $event")
+    event match
+      case fault @ Fault(_, _) =>
+        logger.error(s"*** Handler fault: $fault")
+        store.addFault(fault)
+      case _ =>
+    val eventJson = writeToString[Event](event)
+
+    response
+      .status(200)
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .send(eventJson)
