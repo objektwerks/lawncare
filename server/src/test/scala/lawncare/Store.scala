6007cb4534467065ec45d74cfed866242aec82a2
objektwerks <objektwerks@runbox.com>
2024-04-05 07:34:44 -0400

    enhanced store

--- server/src/test/scala/lawncare/Store.scala
+++ server/src/test/scala/lawncare/Store.scala
@@ -5,7 +5,6 @@ import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import com.zaxxer.hikari.HikariDataSource
 
-import java.time.LocalDate
 import javax.sql.DataSource
 
 import scalikejdbc.*
@@ -20,3 +19,32 @@ object Store:
       .maximumSize(maxSize)
       .expireAfterWrite(expireAfter)
       .build[String, String]()
+
+final class Store(config: Config,
+                  cache: Cache[String, String]) extends LazyLogging:
+  private val dataSource: DataSource = {
+    val ds = new HikariDataSource()
+    ds.setDataSourceClassName(config.getString("db.driverClassName"))
+    ds.addDataSourceProperty("url", config.getString("db.url"))
+    ds.addDataSourceProperty("user", config.getString("db.user"))
+    ds.addDataSourceProperty("password", config.getString("db.password"))
+    ds
+  }
+  ConnectionPool.singleton(new DataSourceConnectionPool(dataSource))
+
+  def isAuthorized(license: String): Boolean =
+    cache.getIfPresent(license) match
+      case Some(_) =>
+        logger.debug(s"*** store cache get: $license")
+        true
+      case None =>
+        val optionalLicense = DB readOnly { implicit session =>
+          sql"select license from account where license = $license"
+            .map(rs => rs.string("license"))
+            .single()
+        }
+        if optionalLicense.isDefined then
+          cache.put(license, license)
+          logger.debug(s"*** store cache put: $license")
+          true
+        else false
