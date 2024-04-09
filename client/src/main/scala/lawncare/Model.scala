c0a7201455f44807033d8d156f94453d87a0e1dc
objektwerks <objektwerks@runbox.com>
2024-04-09 07:34:25 -0400

    enhanced model

--- client/src/main/scala/lawncare/Model.scala
+++ client/src/main/scala/lawncare/Model.scala
@@ -14,5 +14,11 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
   val registered = ObjectProperty[Boolean](true)
   val loggedin = ObjectProperty[Boolean](true)
 
-  val selectedWalkerId = ObjectProperty[Long](0)
+  val selectedPropertyId = ObjectProperty[Long](0)
   val selectedSessionId = ObjectProperty[Long](0)
+
+  selectedPropertyId.onChange { (_, oldPropertyId, newPropertyId) =>
+    logger.info("*** selected property id onchange event: {} -> {}", oldPropertyId, newPropertyId)
+    shouldBeInFxThread("*** selected property id onchange should be in fx thread.")
+    sessions(newPropertyId)
+  }
