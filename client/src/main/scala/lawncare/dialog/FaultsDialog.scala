6fdea4edefd77eea1d0c88baae089dbd29adf67d
objektwerks <objektwerks@runbox.com>
2024-04-09 08:12:30 -0400

    added faults dialog

---
+++ client/src/main/scala/lawncare/dialog/FaultsDialog.scala
@@ -0,0 +1,32 @@
+package lawncare.dialog
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog, TableColumn, TableView}
+import scalafx.scene.layout.VBox
+
+import lawncare.{Client, Context, Fault, Model}
+
+final class FaultsDialog(context: Context, model: Model) extends Dialog[Unit]:
+  initOwner(Client.stage)
+  title = context.windowTitle
+  headerText = context.dialogFaults
+
+  val tableView = new TableView[Fault]():
+    columns ++= List(
+      new TableColumn[Fault, String]:
+        text = context.headerOccurred
+        cellValueFactory = _.value.occurredProperty
+      ,
+      new TableColumn[Fault, String]:
+        prefWidth = 600
+        text = context.headerFault
+        cellValueFactory = _.value.causeProperty
+    )
+    items = model.observableFaults
+
+  dialogPane().buttonTypes = List(ButtonType.Close)
+  dialogPane().content = new VBox:
+    prefWidth = 600
+    prefHeight = 200
+    spacing = 6
+    children = List(tableView)
