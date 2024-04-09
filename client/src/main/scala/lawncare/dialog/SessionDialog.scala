64cdefa320645e2fab4467ce0870f6df81e08db1
objektwerks <objektwerks@runbox.com>
2024-04-09 17:05:37 -0400

    enhanced session dialog

--- client/src/main/scala/lawncare/dialog/SessionDialog.scala
+++ client/src/main/scala/lawncare/dialog/SessionDialog.scala
@@ -12,3 +12,58 @@ final class SessionDialog(context: Context, session: Session) extends Dialog[Ses
   initOwner(Client.stage)
   title = context.windowTitle
   headerText = context.dialogSession
+
+  val weightTextField = new IntTextField:
+    text = session.weight.toString
+  
+  val weightUnitComboBox = new ComboBox[String]:
+  	items = ObservableBuffer.from( WeightUnit.toList )
+  	value = session.weightUnit.toString
+  weightUnitComboBox.prefWidth = 200
+
+  val distanceTextField = new DoubleTextField:
+    text = session.distance.toString
+
+  val distanceUnitComboBox = new ComboBox[String]:
+  	items = ObservableBuffer.from( DistanceUnit.toList )
+  	value = session.distanceUnit.toString
+  distanceUnitComboBox.prefWidth = 150
+
+  val hoursTextField = new IntTextField:
+    text = session.hours.toString
+
+  val minutesTextField = new IntTextField:
+    text = session.minutes.toString
+
+  val caloriesTextField = CalorieTextField(session)
+
+  val datetimeSelector = DateTimeField( context, Entity.toLocalDateTime(session.datetime) )
+
+  val controls = List[(String, Region)](
+    context.labelWeightUnit   -> weightTextField,
+    context.labelWeightUnit   -> weightUnitComboBox,
+    context.labelDistance     -> distanceTextField,
+    context.labelDistanceUnit -> distanceUnitComboBox,
+    context.labelHours        -> hoursTextField,
+    context.labelMinutes      -> minutesTextField,
+    context.labelCalories     -> caloriesTextField,
+    context.labelDatetime     -> datetimeSelector
+  )
+  dialogPane().content = ControlGridPane(controls)
+
+  val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
+  dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
+
+  resultConverter = dialogButton =>
+    if dialogButton == saveButtonType then
+      session.copy(
+        weight = weightTextField.int(session.weight),
+        weightUnit = weightUnitComboBox.value.value,
+        distance = distanceTextField.double(session.distance),
+        distanceUnit = distanceUnitComboBox.value.value,
+        hours = hoursTextField.int(session.hours),
+        minutes = minutesTextField.int(session.minutes),
+        calories = caloriesTextField.int(session.calories),
+        datetime = Entity.toEpochMillis(datetimeSelector.value.value)
+      )
+    else null
