698419cda13be41a33dbc993e392ad44b29ede43
objektwerks <objektwerks@runbox.com>
2024-04-09 07:58:55 -0400

    added model to view

--- client/src/main/scala/lawncare/View.scala
+++ client/src/main/scala/lawncare/View.scala
@@ -4,7 +4,7 @@ import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.VBox
 
-final class View(context: Context):
+final class View(context: Context, model: Model):
   val vbox = new VBox:
     prefWidth = context.windowWidth
     prefHeight = context.windowHeight
