1ac3aa3184ab91fbaf5620df54675d12cab475c4
objektwerks <objektwerks@runbox.com>
2024-04-09 08:17:32 -0400

    added account dialog

---
+++ client/src/main/scala/lawncare/dialog/AccountDialog.scala
@@ -0,0 +1,20 @@
+package lawncare.dialog
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog, Label}
+
+import lawncare.{Account, Client, Context}
+
+final class AccountDialog(context: Context, account: Account) extends Dialog[Unit]:
+  initOwner(Client.stage)
+  title = context.windowTitle
+  headerText = context.dialogAccount
+
+  val controls = List[(String, Label)](
+    context.labelLicense -> Label( account.license ),
+    context.labelEmail -> Label( account.email ),
+    context.labelPin -> Label( account.pin )
+  )
+
+  dialogPane().content = ControlGridPane(controls)
+  dialogPane().buttonTypes = List(ButtonType.Close)
