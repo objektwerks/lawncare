c331a77ef378076f99e17e2edf3b4506e6dc7b7a
objektwerks <objektwerks@runbox.com>
2024-04-09 07:58:38 -0400

    enhanced client

--- client/src/main/scala/lawncare/Client.scala
+++ client/src/main/scala/lawncare/Client.scala
@@ -6,21 +6,32 @@ import com.typesafe.scalalogging.LazyLogging
 import scalafx.application.JFXApp3
 
 object Client extends JFXApp3 with LazyLogging:
-  private val conf = ConfigFactory.load("client.conf")
-  private val context = Context(conf)
-  private val fetcher = Fetcher(context)
-  private val model = Model(fetcher)
+  val conf = ConfigFactory.load("client.conf")
+  val context = Context(conf)
+  val fetcher = Fetcher(context)
+  val model = Model(fetcher)
 
   override def start(): Unit =
-    val view = View(context)
+    val view = View(context, model)
     stage = new JFXApp3.PrimaryStage:
       scene = view.scene
       title = context.windowTitle
       minWidth = context.windowWidth
       minHeight = context.windowHeight
       icons.add(context.logo)
-    
-    stage.show()
-    logger.info("*** client started, targeting: {}", context.url)
+
+    stage.hide()
+
+    model.registered.onChange { (_, _, _) =>
+      Alerts.showRegisterAlert(context, stage)
+      logger.error("*** register failed, client stopping ...")
+      sys.exit(-1)
+    }
+
+    model.loggedin.onChange { (_, _, _) =>
+      Alerts.showLoginAlert(context, stage)
+      logger.error("*** login failed, client stopping ...")
+      sys.exit(-1)
+    }
 
   override def stopApp(): Unit = logger.info("*** client stopped.")
