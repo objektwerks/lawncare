d30448015bf1799bc5747fee4ce5cbe5a0401a65
objektwerks <objektwerks@runbox.com>
2024-04-09 07:28:11 -0400

    added fetcher to client

---
+++ client/src/main/scala/lawncare/Fetcher.scala
@@ -0,0 +1,43 @@
+package lawncare
+
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.typesafe.scalalogging.LazyLogging
+
+import io.helidon.webclient.api.WebClient
+
+import scalafx.application.Platform
+
+import scala.util.Try
+import scala.util.control.NonFatal
+
+final class Fetcher(context: Context) extends LazyLogging:
+  val url = context.url
+  val endpoint = context.endpoint
+  val defaultError = context.errorServer
+  val client = WebClient
+    .builder
+    .baseUri(url)
+    .addHeader("Content-Type", "application/json; charset=UTF-8")
+    .addHeader("Accept", "application/json")
+    .build
+
+  logger.info("*** fetcher url: {} endpoint: {}", url, endpoint)
+
+  def fetch(command: Command,
+            handler: Event => Unit): Unit =
+    logger.info("*** fetcher command: {}", command)
+    val commandJson = writeToString[Command](command)
+    Try {
+      val eventJson = client
+        .post(endpoint)
+        .submit(commandJson, classOf[String])
+        .entity
+      val event = readFromString[Event](eventJson)
+      logger.info("*** fetcher event: {}", event)
+      Platform.runLater(handler(event))
+    }.recover {
+      case NonFatal(error) =>
+        val fault = Fault(error, defaultError)
+        logger.error("*** fetcher fault: {}", fault)
+        Platform.runLater(handler(fault))
+    }
