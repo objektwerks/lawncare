f731428099d96ba96209e3cca15bf17a56d3d0f2
objektwerks <objektwerks@runbox.com>
2024-04-10 08:32:24 -0400

    enhanced properties pane

--- client/src/main/scala/lawncare/pane/PropertiesPane.scala
+++ client/src/main/scala/lawncare/pane/PropertiesPane.scala
@@ -1,5 +1,8 @@
 package lawncare.pane
 
+import scalafx.geometry.Insets
+import scalafx.scene.layout.VBox
+
 import lawncare.{Context, Model}
 
 final class PropertiesPane(context: Context, model: Model) extends VBox:
