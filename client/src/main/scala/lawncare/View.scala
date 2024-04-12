669f73d26fd7c8c85cc960e4a9b7dfe701836a60
objektwerks <objektwerks@runbox.com>
2024-04-12 06:33:43 -0400

    enhanced view

--- client/src/main/scala/lawncare/View.scala
+++ client/src/main/scala/lawncare/View.scala
@@ -1,18 +1,32 @@
 package lawncare
 
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Orientation}
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
+import scalafx.scene.control.SplitPane
+import scalafx.scene.layout.{Priority, VBox}
+
+import lawncare.pane.{PropertiesPane, SessionsPane}
 
 final class View(context: Context, model: Model):
-  println(model) // TODO!
-
   val vbox = new VBox:
     prefWidth = context.windowWidth
     prefHeight = context.windowHeight
     padding = Insets(6)
 
-  vbox.children = List()
+  val walkersPane = PropertiesPane(context, model)
+  VBox.setVgrow(walkersPane, Priority.Always)
+
+  val sessionsPane = SessionsPane(context, model)
+  VBox.setVgrow(sessionsPane, Priority.Always)
+
+  val splitPane = new SplitPane {
+    orientation = Orientation.Horizontal
+    items.addAll(walkersPane, sessionsPane)
+  }
+  splitPane.setDividerPositions(0.20, 0.80)
+  VBox.setVgrow(splitPane, Priority.Always)
+
+  vbox.children = List(splitPane)
 
   val scene = new Scene:
     root = vbox
