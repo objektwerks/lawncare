071918bf4bdd65982a031dd538bb0fe21c588f19
objektwerks <objektwerks@runbox.com>
2024-04-12 05:37:24 -0400

    enhanced sessions pane

--- client/src/main/scala/lawncare/pane/SessionsPane.scala
+++ client/src/main/scala/lawncare/pane/SessionsPane.scala
@@ -8,3 +8,5 @@ import lawncare.{Context, Model}
 final class SessionsPane(context: Context, model: Model) extends VBox:
   spacing = 6
   padding = Insets(6)
+
+  val yesOrNo = (bool: Boolean) => if bool then context.columnYes else context.columnNo
