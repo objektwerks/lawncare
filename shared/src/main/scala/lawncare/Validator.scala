217ae562c2540ae1e5515aa6b226c5ab0e8e0eb3
objektwerks <objektwerks@runbox.com>
2024-04-16 15:03:07 -0400

    enhanced validator

--- shared/src/main/scala/lawncare/Validator.scala
+++ shared/src/main/scala/lawncare/Validator.scala
@@ -2,6 +2,7 @@ package lawncare
 
 object Validator:
   extension (value: String)
+    def isEmptyOrNotEmpty: Boolean = value.isEmpty || value.nonEmpty
     def isLicense: Boolean = if value.nonEmpty && value.length == 36 then true else false
     def isPin: Boolean = value.length == 7
     def isEmail: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
