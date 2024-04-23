020c393e70899eb986b93b80fa451876034bf2e6
objektwerks <objektwerks@runbox.com>
2024-04-23 07:54:14 -0400

    enhanced tabbed pane

--- client/src/main/scala/lawncare/pane/TabbedPane.scala
+++ client/src/main/scala/lawncare/pane/TabbedPane.scala
@@ -4,7 +4,23 @@ import scalafx.geometry.Insets
 import scalafx.scene.control.{Tab, TabPane}
 import scalafx.scene.layout.{Priority, VBox}
 
-import pool.{Context, Model}
+import lawncare.{Context, Model}
 
 final class TabbedPane(context: Context, model: Model) extends VBox:
   padding = Insets(6)
+
+  val sessionsTab = new Tab:
+  	text = context.tabSessions
+  	closable = false
+  	content = SessionsPane(context, model)
+
+  val issuesTab = new Tab:
+  	text = context.tabIssues
+  	closable = false
+  	content = IssuesPane(context, model)
+
+  val tabPane = new TabPane:
+    tabs = List(sessionsTab, issuesTab)
+
+  children = List(tabPane)
+  VBox.setVgrow(tabPane, Priority.Always)
