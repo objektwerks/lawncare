ffe805c727f73735228129f5ebdadf4279011a83
objektwerks <objektwerks@runbox.com>
2024-04-24 09:26:52 -0400

    removed tabbed pane insets

--- client/src/main/scala/lawncare/pane/TabbedPane.scala
+++ client/src/main/scala/lawncare/pane/TabbedPane.scala
@@ -1,14 +1,11 @@
 package lawncare.pane
 
-import scalafx.geometry.Insets
 import scalafx.scene.control.{Tab, TabPane}
 import scalafx.scene.layout.{Priority, VBox}
 
 import lawncare.{Context, Model}
 
 final class TabbedPane(context: Context, model: Model) extends VBox:
-  padding = Insets(6)
-
   val sessionsTab = new Tab:
   	text = context.tabSessions
   	closable = false
