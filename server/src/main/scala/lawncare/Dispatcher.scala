dbc19596ad9440ec86c5f13323f9241486dd6c90
objektwerks <objektwerks@runbox.com>
2024-07-24 05:34:40 -0400

    changed dispatcher dispatcher type signature

--- server/src/main/scala/lawncare/Dispatcher.scala
+++ server/src/main/scala/lawncare/Dispatcher.scala
@@ -7,7 +7,7 @@ import Validator.*
 
 final class Dispatcher(store: Store,
                        emailer: Emailer):
-  def dispatch[E <: Event](command: Command): Event =
+  def dispatch(command: Command): Event =
     if !command.isValid then store.addFault( Fault(s"Command is invalid: $command") )
     
     isAuthorized(command) match
