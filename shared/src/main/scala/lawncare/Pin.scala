c58e8831e6d3ed0cca84438c2ea51ee3e161ee34
objektwerks <objektwerks@runbox.com>
2024-07-26 14:09:24 -0400

    refactored pin

--- shared/src/main/scala/lawncare/Pin.scala
+++ shared/src/main/scala/lawncare/Pin.scala
@@ -1,17 +1,25 @@
 package lawncare
 
+import scala.collection.mutable
 import scala.util.Random
 
 object Pin:
+  private val chars = "abcdefghijklmnopqrstuvwxyz"
   private val specialChars = "~!@#$%^&*-+=<>?/:;".toList
   private val random = Random
 
+  private def newChar: Char = chars( random.nextInt(chars.length) )
   private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
 
   def newInstance: String =
-    Random
-      .alphanumeric
-      .take(5)
-      .mkString
-      .prepended(newSpecialChar)
-      .appended(newSpecialChar)
+    val pin = mutable.ArrayBuffer[Char]()
+
+    pin += newSpecialChar
+    pin += newChar.toUpper
+    pin += newChar.toLower
+    pin += newChar.toUpper
+    pin += newChar.toLower
+    pin += newChar.toUpper
+    pin += newSpecialChar
+
+    pin.mkString
