8be6a0b7c19a6d1a52be467eab624845c949d768
objektwerks <objektwerks@runbox.com>
2024-07-27 07:26:46 -0400

    edited pin test

--- shared/src/test/scala/lawncare/PinTest.scala
+++ shared/src/test/scala/lawncare/PinTest.scala
@@ -3,8 +3,6 @@ package lawncare
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import scala.collection.mutable
-
 final class PinTest extends AnyFunSuite with Matchers:
   test("pin"):
     for i <- 1 to 1000 yield Pin.newInstance.length shouldBe 7
