92bad3c034436a8fbe0c118e749a191be95c3c03
objektwerks <objektwerks@runbox.com>
2025-10-16 14:04:50 -0400

    added taskbar to client

--- client/src/main/scala/lawncare/Client.scala
+++ client/src/main/scala/lawncare/Client.scala
@@ -3,6 +3,9 @@ package lawncare
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import java.awt.{Taskbar, Toolkit}
+import java.awt.Taskbar.Feature
+
 import scalafx.application.JFXApp3
 
 import lawncare.dialog.{Alerts, RegisterLogin, RegisterLoginDialog}
@@ -21,6 +24,12 @@ object Client extends JFXApp3 with LazyLogging:
       minWidth = context.windowWidth
       minHeight = context.windowHeight
       icons.add(context.logo)
+
+    if Taskbar.isTaskbarSupported() then
+      val taskbar = Taskbar.getTaskbar()
+      if taskbar.isSupported(Feature.ICON_IMAGE) then
+        val appIcon = Toolkit.getDefaultToolkit.getImage(this.getClass().getResource("/image/logo.png"))
+        taskbar.setIconImage(appIcon)
 
     stage.hide()
 
