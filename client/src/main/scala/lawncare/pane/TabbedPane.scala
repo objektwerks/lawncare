fdad49ffd15dd16f887f774a26db334e8b508caa
objektwerks <objektwerks@runbox.com>
2025-10-17 10:37:26 -0400

    refactored tabbed pane

--- client/src/main/scala/lawncare/pane/TabbedPane.scala
+++ client/src/main/scala/lawncare/pane/TabbedPane.scala
@@ -1,24 +1,13 @@
 package lawncare.pane
 
-import scalafx.scene.control.{Tab, TabPane}
 import scalafx.scene.layout.{Priority, VBox}
 
 import lawncare.{Context, Model}
 
 final class TabbedPane(context: Context,
                        model: Model) extends VBox:
-  val sessionsTab = new Tab:
-  	text = context.tabSessions
-  	closable = false
-  	content = SessionsPane(context, model)
+  val sessionsPane = SessionsPane(context, model)
+  val issuesPane = IssuesPane(context, model)
 
-  val issuesTab = new Tab:
-  	text = context.tabIssues
-  	closable = false
-  	content = IssuesPane(context, model)
-
-  val tabPane = new TabPane:
-    tabs = List(sessionsTab, issuesTab)
-
-  children = List(tabPane)
-  VBox.setVgrow(tabPane, Priority.Always)
+  children = List(sessionsPane, issuesPane)
+  VBox.setVgrow(this, Priority.Always)
