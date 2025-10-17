101a7c7d42bdacae4100c79fb8560866a8230d55
objektwerks <objektwerks@runbox.com>
2025-10-17 10:33:03 -0400

    refactored model property id change listener

--- client/src/main/scala/lawncare/pane/SessionsPane.scala
+++ client/src/main/scala/lawncare/pane/SessionsPane.scala
@@ -104,7 +104,7 @@ final class SessionsPane(context: Context,
   VBox.setVgrow(tableView, Priority.Always)
   VBox.setVgrow(tabPane, Priority.Always)
 
-  model.selectedSessionId.onChange { (_, _, _) =>
+  model.selectedPropertyId.onChange { (_, _, _) =>
     addButton.disable = false
   }
 
